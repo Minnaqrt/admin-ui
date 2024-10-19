a4e6352e3972dc548529aed4dbf6b7ebc3570b51
Minnaqrt <miyaanaa45@gmail.com>
2024-10-20 06:44:48 +0700

    tambah router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+
+
+// const App = () => {
+// 	return (
+// 		<>
+// 			<SignUpPage/>
+// 		</>
+// 	);
+//   };
+  
+//   export default App
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/signIn";
 import SignUpPage from "./pages/signUp";
+import ErrorRoute from "./pages/errorRoute";
 
 const App = () => {
-	return (
-		<>
-			<SignUpPage/>
-		</>
-	);
-  };
-  
-  export default App
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Halaman Utama</div>,
+	  errorElement: <ErrorRoute />,
+    },
+	{
+		path: "/login",
+		element: <SignInPage />,
+	},
+	{
+		path: "/register",
+		element: <SignUpPage />,
+	},
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+};
+
+export default App;
