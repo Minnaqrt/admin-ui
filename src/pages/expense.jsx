0361f6eb55b32367f6222d6f08873664337d49b2
Minnaqrt <miyaanaa45@gmail.com>
2024-11-04 17:05:40 +0700

    tambah menu expenses

--- src/pages/expense.jsx
+++ src/pages/expense.jsx
@@ -15,17 +15,16 @@ const ExpensePage = () => {
           title="Expenses Breakdown" 
           desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptatum dolorem impedit quam hic dolore nam eos, corporis qui consectetur placeat id, vitae rem magni repudiandae reprehenderit maiores veniam saepe!"
         />
-        <Card title="&nbsp;" />
-        <Card 
-          title="&nbsp;"
-          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis possimus excepturi temporibus deserunt modi nam velit quos, ad facere dignissimos in accusamus, libero doloribus impedit ab. Culpa saepe dignissimos explicabo?"
+        <Card title="&nbsp;"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis possimus excepturi temporibus deserunt modi nam velit quos, ad facere dignissimos in accusamus, libero doloribus impedit ab. Culpa saepe dignissimos explicabo?" />
+        <Card title="&nbsp;"
         />
       </div>
       </div>
       <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <Card />
+        <Card />
         <Card desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, eaque. Sed, voluptatibus officiis earum maxime deserunt quia eligendi, deleniti fugit vitae accusamus porro sequi. Debitis quos in ut? Provident, magni." />
-        <Card />
-        <Card />
       </div>
       {/* bottom content end*/}
     </MainLayout>
