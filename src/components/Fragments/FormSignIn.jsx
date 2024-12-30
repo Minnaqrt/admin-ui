9de96ee7198568df9c5ed33f5e41c02da26376b9
Minnaqrt <miyaanaa45@gmail.com>
2024-12-30 17:16:17 +0700

    tambah get card data goals, animasi, cypress

--- src/components/Fragments/FormSignIn.jsx
+++ src/components/Fragments/FormSignIn.jsx
@@ -1,121 +1,124 @@
-
-import LabeledInput from '../Elements/LabeledInput'
-import CheckBox from '../Elements/CheckBox'
-import Button from '../Elements/Button'
-import { useForm } from 'react-hook-form'
-import axios from 'axios'
-import { useContext, useState } from 'react'
-import CustomizedSnackbars from '../Elements/SnackBar'
-import { jwtDecode } from 'jwt-decode'
-import { useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../context/authContext'
+import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import LabeledInput from '../Elements/LabeledInput';
+import CheckBox from '../Elements/CheckBox';
+import Button from '../Elements/Button';
+import CustomizedSnackbars from '../Elements/SnackBar';
+import { AuthContext } from '../../context/authContext';
+import { NotifContext } from '../../context/notifContext';
 
 const FormSignIn = () => {
-  const [msg, setMsg] = useState("")
-  const [open, setOpen] = useState(true)
-  const { setIsLoggedIn, setName } = useContext(AuthContext)
+  const { msg, setMsg, open, setOpen, isLoading, setIsLoading } = useContext(NotifContext);
+  const { setIsLoggedIn, setName } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm ({ mode: "onChange",})
+  } = useForm({ mode: 'onChange' });
 
-  const onErrors = (errors) => console.error(errors)
+  const onErrors = (errors) => console.error(errors);
 
   const onFormSubmit = async (data) => {
+    setIsLoading(true);
     try {
       const response = await axios.post(
-        "https://jwt-auth-eight-neon.vercel.app/login",
+        'https://jwt-auth-eight-neon.vercel.app/login',
         {
           email: data.email,
           password: data.password,
         }
-      )
-      
+      );
 
-      const decoded = jwtDecode(response.data.refreshToken)
-      console.log(decoded)
+      setIsLoading(false);
+      setOpen(true);
+      setMsg({ severity: 'success', desc: 'Login Success' });
 
-      setOpen(true)
-      setMsg({ severity: "success", desc: "Login Success" })
+      setIsLoggedIn(true);
+      localStorage.setItem('refreshToken', response.data.refreshToken);
 
-      localStorage.setItem("refreshToken", response.data.refreshToken)
+      const decoded = jwtDecode(response.data.refreshToken);
+      setName(decoded.name);
 
-      setIsLoggedIn(true)
-      setName(decoded.name)
+      console.log(decoded);
 
-      navigate("/")
+      navigate('/');
     } catch (error) {
+      setIsLoading(false);
+
       if (error.response) {
-        setOpen(true)
-        setMsg({ severity: "error", desc: error.response.data.msg })
+        setOpen(true);
+        setMsg({ severity: 'error', desc: error.response.data.msg });
       }
     }
-  }
+  };
 
   return (
-			<form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
-			  <div className="mb-6">
-				<LabeledInput
-                label="Email Address"
-                type="email"
-                placeholder="hello@example.com"
-                name="email"
-                register={{
-                  ...register("email", {
-                    required: "Email address is required",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9._%+-]+\.[A-Z]{2,}$/i,
-                      message: "Invalid email address format",
-                    }
-                  })
-                }}
-                />
-                {errors?.email && (
-                  <div className="text-center text-red-500">{errors.email.message}</div>
-                )}
-			  </div>
-			  <div className="mb-6">
-                <LabeledInput
-                label="Password"
-                type="password"
-                placeholder="*************"
-                name="password"
-                register={{
-                  ...register("password", { required: "Paswword is required" })
-                }}
-                />
-                {errors?.password && (
-                  <div className="text-center text-red-500">{errors.password.message}</div>
-                )}
-			  </div>
-			  <div className="mb-6">
-				<CheckBox label="Keep me signed in" name="status"/>
-			  </div>
-			  <Button 
-          variant={
-            !isValid
-              ? "bg-gray-05 w-full text-white" 
-              : "bg-primary w-full text-white"
-          }
-          type="submit"
-          disabled={!isValid ? "disabled" : ""}
-        >
-            Login
-        </Button>
-        {msg && (
-          <CustomizedSnackbars
-            severity={msg.severity}
-            message={msg.desc}
-            open={open}
-            setOpen={setOpen}
-          />
+    <form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
+      <div className="mb-6">
+        <LabeledInput
+          label="Email Address"
+          type="email"
+          placeholder="hello@example.com"
+          name="email"
+          register={{
+            ...register('email', {
+              required: 'Email address is required',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9._%+-]+\.[A-Z]{2,}$/i,
+                message: 'Invalid email address format',
+              },
+            }),
+          }}
+        />
+        {errors?.email && (
+          <div className="text-center text-red-500">
+            {errors.email.message}
+          </div>
         )}
-			</form>
-  )
-}
+      </div>
+      <div className="mb-6">
+        <LabeledInput
+          label="Password"
+          type="password"
+          placeholder="*************"
+          name="password"
+          register={{
+            ...register('password', { required: 'Password is required' }),
+          }}
+        />
+        {errors?.password && (
+          <div className="text-center text-red-500">
+            {errors.password.message}
+          </div>
+        )}
+      </div>
+      <div className="mb-6">
+        <CheckBox label="Keep me signed in" name="status" />
+      </div>
+      <Button
+        variant={`${!isValid ? "bg-gray-05" : "bg-primary zoom-in"} 
+                  w-full text-white`}
+        type="submit"
+        disabled={!isValid ? "disabled" : ""}
+      >
+        Login
+      </Button>
+      {msg && (
+        <CustomizedSnackbars
+          severity={msg.severity}
+          message={msg.desc}
+          open={open}
+          setOpen={setOpen}
+        />
+      )}
+    </form>
+  );
+};
 
-export default FormSignIn
+export default FormSignIn;
--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -2,26 +2,41 @@ import { useContext } from "react";
 import Header from "../Fragments/Header";
 import Navbar from "../Fragments/Navbar";
 import { ThemeContext } from "../../context/themeContext";
+import SimpleBackdrop from '../Elements/Backdrop';
+import CustomizedSnackbars from '../Elements/SnackBar';
+import { NotifContext } from "../../context/notifContext";
 
 const MainLayout = (props) => {
-    const { children } = props
-    const { theme } = useContext(ThemeContext)
+  const { children } = props;
+  const { theme } = useContext(ThemeContext);
+  const { msg, setMsg, open, setOpen, isLoading, setIsLoading } = useContext(NotifContext);
 
-    return (
-        <div className={`flex bg-special-mainBg w-screen min-h-screen max-w-full ${theme.name}`}>
-        {/* navbar start*/}
-        <Navbar />
-        {/* navbar end*/}
-        <div className="w-screen">
-          {/* header start*/}
-          <Header />
-          {/* header end*/}
-          {/* content start*/}
-          <main className="px-6 py-4">{children}</main>
-          {/* content end*/}
-        </div>
+  return (
+    <div className={`flex bg-special-mainBg w-screen min-h-screen max-w-full ${theme?.name}`}>
+      {/* navbar start */}
+      <Navbar />
+      {/* navbar end */}
+      <div className="w-screen">
+        {isLoading && (
+          <SimpleBackdrop isLoading={isLoading} setIsLoading={setIsLoading} />
+        )}
+        {msg && (
+          <CustomizedSnackbars
+            severity={msg.severity}
+            message={msg.desc}
+            open={open}
+            setOpen={setOpen}
+          />
+        )}
+        {/* header start */}
+        <Header />
+        {/* header end */}
+        {/* content start */}
+        <main className="px-6 py-4">{children}</main>
+        {/* content end */}
       </div>
-    );
-  };
-  
-  export default MainLayout;
+    </div>
+  );
+};
+
+export default MainLayout;
